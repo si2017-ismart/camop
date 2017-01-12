531ea55d0b4cb2a50dbe162f89013a71228402ea
HaithemK <krifa.haithem@gmail.com>
2017-01-12 19:49:51 +0100

    init

---
+++ app/src/main/java/com/mynewproject/MainActivity.java
@@ -0,0 +1,34 @@
+package com.mynewproject;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageButton button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stream);
+        button = (ImageButton) findViewById(R.id.Buttonj);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, VideoViewActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
+
+
+
+
+
---
+++ app/src/main/java/com/mynewproject/VideoViewActivity.java
@@ -0,0 +1,53 @@
+package com.mynewproject;
+
+import android.app.ProgressDialog;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class VideoViewActivity extends AppCompatActivity {
+    ProgressDialog pDialog;
+    VideoView videoview;
+
+    String VideoURL="http://192.168.12.158:8080";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_view);
+
+        videoview = (VideoView) findViewById(R.id.VideoView);
+        pDialog = new ProgressDialog(VideoViewActivity.this);
+
+        pDialog.setTitle("Video streaming");
+        pDialog.setMessage("Loading...");
+        pDialog.setIndeterminate(false);
+        pDialog.setCancelable(false);
+        pDialog.show();
+
+        try{
+            MediaController mediaController = new MediaController(VideoViewActivity.this);
+            mediaController.setAnchorView(videoview);
+            Uri video = Uri.parse(VideoURL);
+            videoview.setMediaController(mediaController);
+            videoview.setVideoURI(video);
+
+        }
+        catch (Exception e){
+            Log.e("Error",e.getMessage());
+            e.printStackTrace();
+
+        }
+        videoview.requestFocus();
+        videoview.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                pDialog.dismiss();
+                videoview.start();
+            }
+        });
+    }
+}
